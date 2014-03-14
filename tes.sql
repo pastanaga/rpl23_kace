6fca5682c9c3ad1d1eaef2da28b08da23b4c91d5
randywiratma <randywiratma@users.noreply.github.com>
2014-03-14 14:37:05 +0700

    Update tes.sql

--- tes.sql
+++ tes.sql
@@ -9,7 +9,8 @@
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110183','Aghnat Fikkri Rakarisa','IF-5','aghfi');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110199','Muhammad Lukman','IF-5','muhammadlukman099');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110198','Andri Sandria Yuwana','IF-5','andryabelz');
-	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110184','Andri Sandria Yuwana','IF-5','tabri adjah');
+	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('101101210','Randy Priyambodo Wiratma','IF-5','Randywiratma');
+
  	/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
  	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
  	/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
