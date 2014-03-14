abf003660c72def79c190455ed7571a7c9f08da2
Backrtz12 <ridwanzhanzuke@gmail.com>
2014-03-14 17:01:34 +0700

    update tes2.sql
    update deui

--- tes.sql
+++ tes.sql
@@ -10,7 +10,7 @@
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110199','Muhammad Lukman','IF-5','muhammadlukman099');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110198','Andri Sandria Yuwana','IF-5','andryabelz');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110210','Randy Priyambodo Wiratma','IF-5','Randywiratma');
-
+        insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110189','Ridwan Effendi','IF-5','Backrtz12');
  	/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
  	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
  	/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
