6c3a73422c1a755c4c00773efd0d3855916d82a0
muhamadlukman099 <look_mon07@ymail.com>
2014-03-14 13:13:15 +0700

    Update tes.sql
    test

--- tes.sql
+++ tes.sql
@@ -7,7 +7,8 @@
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10111203','Muhammad Fajar Pratama','IF-5','pastanaga');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10111203','ogud','IF-5','pastanaga');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110183','Aghnat Fikkri Rakarisa','IF-5','aghfi');
-	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110','Muhammad Lukman','IF-5','muhammadlukman099');
+	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110199','Muhammad Lukman','IF-5','muhammadlukman099');
+	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110198','Andri Sandria Yuwana','IF-5','andryabelz');
  	/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
  	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
  	/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
