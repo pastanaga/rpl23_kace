bdf8f1cc9eb2c78cf03fd34710e2f405c69d3339
Aghnat Fikkri <last_forgotten@rocketmail.com>
2014-03-14 12:51:20 +0700

    Update test V-0.1
    nambah biodata Aghnat

--- tes.sql
+++ tes.sql
@@ -6,6 +6,7 @@
 +	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10111203','Muhammad Fajar Pratama','IF-5','pastanaga');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10111203','Muhammad Fajar Pratama','IF-5','pastanaga');
 	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10111203','ogud','IF-5','pastanaga');
+	insert  into `rplduatiga`(`nim`,`nama`,`kelas`,`username`) values ('10110183','Aghnat Fikkri Rakarisa','IF-5','aghfi');
  	/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
  	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
  	/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
